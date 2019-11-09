1b6911caacfc650d35bba86579804fbfc1878fdd
gabrieldpg <gabrielpcpg@gmail.com>
2019-11-10 11:26:12 +1300

    added event handler for closing window

--- FlappyBird/FlappyBird/main.cpp
+++ FlappyBird/FlappyBird/main.cpp
@@ -3,16 +3,28 @@
 #define SCREEN_WIDTH 512
 #define SCREEN_HEIGHT 512
 
-int main() {
+int main() 
+{
 	sf::RenderWindow window(sf::VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT), "Flappy Bird");
 
-	while (window.isOpen()) {
+	while (window.isOpen()) 
+	{
 		// handle events
+		sf::Event event;
+
+		while (window.pollEvent(event))
+		{
+			switch (event.type)
+			{
+			case sf::Event::Closed:
+				window.close();
+				break;
+			}
+		}
 
 		// update game logic
-
-		// draw
 		window.clear();
+
 		// draw SFML objects
 		window.display();
 	}
