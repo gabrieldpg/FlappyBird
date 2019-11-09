030dcd6ed6f0a5026c86c6188705e6a0ba4567c6
gabrieldpg <gabrielpcpg@gmail.com>
2019-11-10 11:53:26 +1300

    state and state machine created

--- FlappyBird/FlappyBird/main.cpp
+++ FlappyBird/FlappyBird/main.cpp
@@ -1,33 +1,6 @@
-#include <SFML/Graphics.hpp>
-
-#define SCREEN_WIDTH 512
-#define SCREEN_HEIGHT 512
+#include <iostream>
 
 int main() 
 {
-	sf::RenderWindow window(sf::VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT), "Flappy Bird");
-
-	while (window.isOpen()) 
-	{
-		// handle events
-		sf::Event event;
-
-		while (window.pollEvent(event))
-		{
-			switch (event.type)
-			{
-			case sf::Event::Closed:
-				window.close();
-				break;
-			}
-		}
-
-		// update game logic
-		window.clear();
-
-		// draw SFML objects
-		window.display();
-	}
-
 	return EXIT_SUCCESS;
 }
